7f340368cda154e8aab954ab28372652b0489bd1
SOHEE LEE <163387330+0isohee@users.noreply.github.com>
2024-09-03 18:14:49 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -1,16 +1,16 @@
 #include<iostream>
-#include<cmath>
+
 using namespace std;
 
-int main(){
+int main() {
     
     int T;
     cin >> T;
-    for (int i=0; i<T; i++){
-        int A, B;
-        cin >> A >> B;
-        cout << "Case #" << i+1 << ": " << A+B << "\n";
-    }
     
+    for (int i=0; i<T; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << "Case #" << i+1 <<": " <<a+b << "\n";
+    }
     return 0;
 }
